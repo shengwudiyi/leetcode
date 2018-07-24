08b6f4549c83ebdf8fbce943a93253a54b061942
shengwudiyi <swdy@shengwudiyideMacBook-Pro.local>
2018-07-24 20:29:00 +0800

    leetcode 75

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ LeetCode
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+| 75 | [Sort Colors](https://leetcode-cn.com/problems/sort-colors/description/) | [Python](./algorithms/python3/SortColors.py) | Medium | 
 | 1 | [Two Sum](https://leetcode-cn.com/problems/two-sum/description/) | [Go](./algorithms/golang/TwoSum.go) | Easy |
---
+++ algorithms/python3/SortColors.py
@@ -0,0 +1,25 @@
+"""
+Source	    : https://leetcode-cn.com/problems/sort-colors/description/
+Author	    : Lianfeng Shen
+Date	    : 2018-07-24
+"""
+
+class Solution:
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums) - 1
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                i += 1
+                l += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
