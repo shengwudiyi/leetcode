b4fe021aa9de248c579f582080468dea3c268c52
shengwudiyi <swdy@shengwudiyideMacBook-Pro.local>
2018-08-01 16:56:42 +0800

    add cpp

---
+++ algorithms/cpp/Contains_Duplicate_II.cpp
@@ -0,0 +1,19 @@
+// Source	    : https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+// Author	    : Lianfeng Shen
+// Date         : 2018-08-01
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        int n;
+        for (int i=0; i<nums.size(); i++) {
+            n = nums[i];
+            if (m.find(n) != m.end() && i-m[n] <= k) {
+                return true;
+            }
+            m[n] = i;
+        }
+        return false;
+    }   
+};
