41b818df69ab80aec9e7b8c1e018c824f4833e11
shengwudiyi <17091600416@163.com>
2019-04-25 20:39:02 +0800

    fix

--- algorithms/golang/1.TwoSum.go
+++ algorithms/golang/1.TwoSum.go
@@ -1,6 +1,14 @@
-// Source:	https://leetcode-cn.com/problems/two-sum/description/
-// Author:	Lianfeng Shen
-// Date:	2019-04-25
+// Source :	https://leetcode-cn.com/problems/two-sum/description/
+// Author :	Lianfeng Shen
+// Date   :	2019-04-25
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
 
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
