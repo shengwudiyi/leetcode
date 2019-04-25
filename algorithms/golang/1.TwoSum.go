5f8ea1a3b8c46ab2c086ab4d93d17904842ab982
shengwudiyi <17091600416@163.com>
2019-04-25 20:12:03 +0800

    leetcode 1 golang

---
+++ algorithms/golang/1.TwoSum.go
@@ -0,0 +1,17 @@
+// Source:	https://leetcode-cn.com/problems/two-sum/description/
+// Authou:	Lianfeng Shen
+// Date:	2019-04-25
+
+
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i, num := range nums {
+        if j, ok := m[target - num]; ok {
+            return []int{j, i}
+        } else {
+            m[num] = i
+        }
+    }
+    
+    return []int{}
+}
