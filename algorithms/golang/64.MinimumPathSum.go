0ca8ec32f7a2747bc049b7e119879ce5e13bc38d
shengwudiyi <17091600416@163.com>
2019-04-29 19:48:00 +0800

    fix

--- algorithms/golang/64.MinimumPathSum.go
+++ algorithms/golang/64.MinimumPathSum.go
@@ -3,27 +3,26 @@
 // Date   : 2019-04-29
 
 func minPathSum(grid [][]int) int {
-    
-    for i := 1; i < len(grid); i++ {
-        grid[i][0] += grid[i-1][0]
-    }
-    
-    for j := 1; j < len(grid[0]); j++ {
-        grid[0][j] += grid[0][j-1]
-    }
-    
-    for i := 1; i < len(grid); i++ {
-        for j := 1; j < len(grid[0]); j++ {
-            grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        }
-    }
-    return grid[len(grid) - 1][len(grid[0]) - 1]
+	for i := 1; i < len(grid); i++ {
+		grid[i][0] += grid[i-1][0]
+	}
+
+	for j := 1; j < len(grid[0]); j++ {
+		grid[0][j] += grid[0][j-1]
+	}
+
+	for i := 1; i < len(grid); i++ {
+		for j := 1; j < len(grid[0]); j++ {
+			grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
 }
 
 func min(x int, y int) int {
-    if x < y {
-        return x
-    } else {
-        return y
-    }
+	if x < y {
+		return x
+	} else {
+		return y
+	}
 }
