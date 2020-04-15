4de5cd401ca63a8dad4893b9e2168819d2395ac7
shengwudiyi <shengwudiyi@gmail.com>
2020-04-15 23:12:19 +0800

    leetcode 48 go

---
+++ algorithms/golang/48.rotate.go
@@ -0,0 +1,17 @@
+// Source : https://leetcode-cn.com/problems/rotate-image/
+// Author : Lianfeng Shen
+// Date   : 2020-04-15
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+	}
+}
